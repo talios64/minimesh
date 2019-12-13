a1bac83e75cfe980dfc2f6f172e16b0a6556a308
VERDIER Thibaud <tverdier001@khalifman.pedago.ipb.fr>
2019-12-13 09:34:57 +0100

    ajout du main modifié

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@
 
 #include "options.hpp"
 #include "viewer.hpp"
+#include "transform.hpp"
+#include "quality.hpp"
 
 void print_usage()
 {
@@ -53,15 +55,41 @@ int main(int argc, char **argv)
     auto viewer = new Viewer();
     viewer->view(reader->GetOutput());
   } else if (command == "-t" || command == "--transform") {
+
+    if (argc < 3) {
+      std::cerr << "Error: missing file\n";
+      print_usage();
+      return EXIT_FAILURE;
+    }
+    // if (TransformType == TRANSFORM_TRANSLATION){
+      file_name = argv[2];
+      auto op = new OptionsParser(file_name);
+      /*auto params = */op->parse();
+
+      auto reader = vtkSmartPointer<vtkXMLUnstructuredGridReader>::New();
+      reader->SetFileName(file_name.c_str());
+      reader->Update();
+
+      auto transformation = new Transform();
+      // transformation->translate(reader->GetOutput(),std::vector<double> coords);
+      delete op;
+    // }
+
+  } else if (command == "-q" || command == "--quality") {
+
     if (argc < 3) {
       std::cerr << "Error: missing file\n";
       print_usage();
       return EXIT_FAILURE;
     }
     file_name = argv[2];
-    auto op = new OptionsParser(file_name);
-    /*auto params = */op->parse();
-    delete op;
+    auto reader = vtkSmartPointer<vtkXMLUnstructuredGridReader>::New();
+    reader->SetFileName(file_name.c_str());
+    reader->Update();
+
+    auto checker = new QualityChecker();
+    checker->Quality_Analysis(reader->GetOutput());
+
   } else {
     std::cerr << "Error: unknown command\n";
     print_usage();
